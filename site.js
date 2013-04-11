2cf3184be1de5b7e5fe085d61c3bf89f476e7610
Darrick J. Wong <djwong@djwong.org>
2013-04-10 21:25:09 -0700

    style/js: Work around IE6/7 bugs.  Really.

--- site.js
+++ site.js
@@ -3,7 +3,12 @@ function crib_toggle(id)
 	var x = document.getElementById(id);
 	var y = document.getElementById(id + "_ctl");
 	if (x.style.display == "") {
-		x.style.display = "table-row";
+		/* work around a bug in ie6/7. */
+		try {
+			x.style.display = "table-row";
+		} catch(e) {
+			x.style.display = "block";
+		}
 		y.innerHTML = "&#9660;";
 	} else {
 		x.style.display = "";
