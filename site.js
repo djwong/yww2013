8f16595aa7bec6b86276e2f23dc1c91c14cb4b61
Darrick J. Wong <djwong@plum.djwong.org>
2013-07-21 23:26:33 -0700

    Convert the (pathetically tiny) JavaScript to use jQuery.

--- site.js
+++ site.js
@@ -1,17 +1,12 @@
 function crib_toggle(id)
 {
-	var x = document.getElementById(id);
-	var y = document.getElementById(id + "_ctl");
-	if (x.style.display == "") {
-		/* work around a bug in ie6/7. */
-		try {
-			x.style.display = "table-row";
-		} catch(e) {
-			x.style.display = "block";
-		}
-		y.innerHTML = "&#9660;";
+	var x = $('#' + id);
+	var y = $('#' + id + "_ctl");
+	if (x.css('display') == 'none') {
+		x.css('display', 'table-row');
+		y.text('▼');
 	} else {
-		x.style.display = "";
-		y.innerHTML = "&#9654;";
+		x.css('display', 'none');
+		y.text('▶');
 	}
 }
